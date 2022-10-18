5ae53e18fd41c13167303ae70df0e3cdbf07bb2e
deberle1 <eberle@live.de>
2022-10-18 16:31:35 +0200

    Commit please

--- Hello_world_ecl_3/src/de/uni/koeln/se/Main.java
+++ Hello_world_ecl_3/src/de/uni/koeln/se/Main.java
@@ -1,5 +1,10 @@
 package de.uni.koeln.se;
 
 public class Main {
+	public static void main(String[] args) {
+        System.out.println("Hello world!1!!!");
+        
+        System.out.println("Goodbye world!1!!!!!");
+    }
 
 }
